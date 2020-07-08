375dc8c00fc893c50cdea8e6484587fe3c14fbe7
Whitney Kidd <whitneymkidd@gmail.com>
2020-07-08 15:18:35 -0600

    Update test to include deleted video expectation

--- spec/features/admin/admin_can_delete_tutorial_spec.rb
+++ spec/features/admin/admin_can_delete_tutorial_spec.rb
@@ -4,6 +4,32 @@ feature "An admin can delete a tutorial" do
   scenario "and it should no longer exist" do
     admin = create(:admin)
     create_list(:tutorial, 2)
+    tutorial = Tutorial.last
+    
+    video1 = Video.create!({
+      "title"=>"Prework - Environment Setup",
+      "description"=> Faker::Hipster.paragraph(2, true),
+      "video_id"=>"qMkRHW9zE1c",
+      "thumbnail"=>"https://i.ytimg.com/vi/qMkRHW9zE1c/hqdefault.jpg",
+      "position"=>1,
+      "tutorial_id"=> tutorial.id
+    })
+    video2 = Video.create!({
+      "title"=>"Prework - SSH Key Setup",
+      "description"=> Faker::Hipster.paragraph(2, true),
+      "video_id"=>"XsPVWGKK0qI",
+      "thumbnail"=>"https://i.ytimg.com/vi/XsPVWGKK0qI/hqdefault.jpg",
+      "position"=>2,
+      "tutorial_id"=> tutorial.id
+    })
+    video3 = Video.create!({
+      "title"=>"Prework - Strings",
+      "description"=> Faker::Hipster.paragraph(2, true),
+      "video_id"=>"iXLwXvev4X8",
+      "thumbnail"=>"https://i.ytimg.com/vi/iXLwXvev4X8/hqdefault.jpg",
+      "position"=>3,
+      "tutorial_id"=> tutorial.id
+    })
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
@@ -15,5 +41,15 @@ feature "An admin can delete a tutorial" do
       click_link 'Delete'
     end
     expect(page).to have_css('.admin-tutorial-card', count: 1)
+    expect(Tutorial.all).to_not include(tutorial)
+    expect(Video.all).to_not include(video1, video2, video3)
   end
 end
+
+
+# Background: Currently deleting a
+# Tutorial will leave all of the videos in the database,
+# meaning they will be referencing a tutorial that doesn't exist.
+
+# Use ActiveRecords dependent destroy
+# functionality to fix this.
