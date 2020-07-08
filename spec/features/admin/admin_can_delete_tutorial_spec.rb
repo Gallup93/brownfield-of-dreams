641f8a6bc28cdbc8d9ee1902153356b0c75a4534
Whitney Kidd <whitneymkidd@gmail.com>
2020-07-08 15:31:49 -0600

    add dependent::destroy functionality to videos/tutorials

--- spec/features/admin/admin_can_delete_tutorial_spec.rb
+++ spec/features/admin/admin_can_delete_tutorial_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 feature "An admin can delete a tutorial" do
   scenario "and it should no longer exist" do
     admin = create(:admin)
-    create_list(:tutorial, 2)
-    tutorial = Tutorial.last
+    
+    tutorial1 = create(:tutorial)
+    tutorial2 = create(:tutorial)
     
     video1 = Video.create!({
       "title"=>"Prework - Environment Setup",
@@ -12,23 +13,25 @@ feature "An admin can delete a tutorial" do
       "video_id"=>"qMkRHW9zE1c",
       "thumbnail"=>"https://i.ytimg.com/vi/qMkRHW9zE1c/hqdefault.jpg",
       "position"=>1,
-      "tutorial_id"=> tutorial.id
+      "tutorial_id"=> tutorial1.id
     })
+
     video2 = Video.create!({
       "title"=>"Prework - SSH Key Setup",
       "description"=> Faker::Hipster.paragraph(2, true),
       "video_id"=>"XsPVWGKK0qI",
       "thumbnail"=>"https://i.ytimg.com/vi/XsPVWGKK0qI/hqdefault.jpg",
       "position"=>2,
-      "tutorial_id"=> tutorial.id
+      "tutorial_id"=> tutorial1.id
     })
+
     video3 = Video.create!({
       "title"=>"Prework - Strings",
       "description"=> Faker::Hipster.paragraph(2, true),
       "video_id"=>"iXLwXvev4X8",
       "thumbnail"=>"https://i.ytimg.com/vi/iXLwXvev4X8/hqdefault.jpg",
       "position"=>3,
-      "tutorial_id"=> tutorial.id
+      "tutorial_id"=> tutorial2.id
     })
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
@@ -41,8 +44,9 @@ feature "An admin can delete a tutorial" do
       click_link 'Delete'
     end
     expect(page).to have_css('.admin-tutorial-card', count: 1)
-    expect(Tutorial.all).to_not include(tutorial)
-    expect(Video.all).to_not include(video1, video2, video3)
+    expect(Tutorial.all).to_not include(tutorial1)
+    expect(Video.all).to_not include(video1, video2)
+    expect(Video.all).to include(video3)
   end
 end
 
